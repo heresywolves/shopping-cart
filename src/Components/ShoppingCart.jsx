48feb65900d0d8c8fc3bd310fedc063902f2a93b
Howl Kimura <howlkimuradev@gmail.com>
2024-01-06 17:16:13 +0300

    Display items in the shopping cart

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,10 +1,29 @@
 import { Link } from "react-router-dom";
+import { ShopContext } from "../Context/ShopContext";
+import { useContext } from "react";
+import products from "../products";
+import ProductCard from "./ProductCard";
 
 const ShoppingCart = () => {
+
+  const { 
+    addToCart,
+    subtractFromCart, 
+    cartItems,
+    setQty,
+  } = useContext(ShopContext);
+
   return (
     <div>
       <h1>Shopping cart</h1>
       <Link to="/">Back to shopping</Link>
+      {products.map((item) => {
+        if (cartItems[item.id] > 0) {
+          return (
+            <ProductCard key={item.id} item={item}/>
+          )
+        }
+      })}
     </div>
   )
 };
