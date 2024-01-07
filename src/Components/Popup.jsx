3417c7717d571d62bab16d933ceb623d068a4e71
Howl Kimura <howlkimuradev@gmail.com>
2024-01-07 16:01:13 +0300

    style popup window

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -1,14 +1,16 @@
+import './Popup.css';
 
 function Popup ({content, confirmAction}) {
 
   return (
     <div className="popup-container">
       <div className="popup">
+        <div></div>
         <p>{content}</p>
         <button 
         className='popup-confirm-button'
         onClick={confirmAction}
-        >Ok</button>
+        >OK</button>
       </div>
     </div>
   )
